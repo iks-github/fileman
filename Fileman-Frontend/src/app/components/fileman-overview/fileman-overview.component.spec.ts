d1cdacc6acb2195ad07b07ef6469fc36eefa866b
Schäfer <asc@iks-gmbh.com>
2020-05-29 11:30:51 +0200

    [#18] test file deletion in FilemanOverviewComponent

--- Fileman-Frontend/src/app/components/fileman-overview/fileman-overview.component.spec.ts
+++ Fileman-Frontend/src/app/components/fileman-overview/fileman-overview.component.spec.ts
@@ -13,11 +13,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 import { FilemanOverviewComponent } from './fileman-overview.component';
+import { FilemanMetadataService } from 'src/app/services/fileman-metadata-service.service';
+import { FilemanFileService } from 'src/app/services/fileman-file-service.service';
+import { FileMetaData } from 'src/app/common/domainobjects/gen/FileMetaData';
 
 describe('FilemanOverviewComponent', () => {
   let component: FilemanOverviewComponent;
@@ -40,4 +44,36 @@ describe('FilemanOverviewComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should delete file',
+      inject([FilemanMetadataService, FilemanFileService],
+      (metadataService: FilemanMetadataService, fileService: FilemanFileService) => {
+
+    const toBeDeleted: FileMetaData = new FileMetaData({name: 'to_be_deleted.txt'});
+    const toPersist: FileMetaData = new FileMetaData({name: 'to_persist.txt'});
+
+    // fake server call in metadataService should return 2 test files
+    spyOn<any>(metadataService, 'getOverviewDataFromServer').and.returnValue(
+      new Observable(stream => {
+        stream.next([toBeDeleted, toPersist]);
+        stream.complete();
+    }));
+
+    component.onReloadClick();
+
+    // before deletion: 2 files
+    expect(component.viewedFiles.length).toEqual(2);
+    expect(component.viewedFiles[0].getName()).toEqual('to_be_deleted.txt');
+    expect(component.viewedFiles[1].getName()).toEqual('to_persist.txt');
+
+    spyOn(window, 'confirm').and.returnValue(true);
+    spyOn(fileService, 'delete').and.returnValue(
+      new Observable(() => {
+        // after deletion: only 1 file left (the one that was not deleted)
+        expect(component.viewedFiles.length).toEqual(1);
+        expect(component.viewedFiles[0].getName()).toEqual('to_persist.txt');
+    }));
+
+    component.delete(toBeDeleted);
+  }));
 });
